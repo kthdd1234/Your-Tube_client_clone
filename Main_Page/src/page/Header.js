6e9c0802f81a8571566da149209e2484ac8484bb
jhin93 <scene1993@gmail.com>
2020-05-29 05:15:10 +0900

    완성된 css

--- Main_Page/src/page/Header.js
+++ Main_Page/src/page/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../logo.png';
+import './Header.css';
 
-const Header = ({handleSettingsToggle}) => {
+const Header = ({ handleSettingsToggle }) => {
   return (
     <div>
-      <div className='YourTube'>
-        <img src={logo} alt='YoutubeLogo' className='HeaderLogo' />
-        YourTube
+      <div className="YourTube">
+        <img src={logo} alt="YoutubeLogo" className="HeaderLogo" />
+        <h3 className="Logo">YourTube</h3>
       </div>
-      <button className='modalButton' onClick={handleSettingsToggle}>
+      <button className="modalButton" onClick={handleSettingsToggle}>
         &#8801; Settings
       </button>
     </div>
--- Main_Page/src/page/VideoList.js
+++ Main_Page/src/page/VideoList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Video from './Video';
+import './Video.css';
 
-const VideoList = ({videos, profile}) => {
+const VideoList = ({ videos, profile }) => {
   return (
     <>
-      <h3> {profile.name}님의 좋아요 동영상 리스트입니다.</h3>
-      <div className='media'>
+      <h3 className="whosid">{profile.name}님의 좋아요 동영상 리스트입니다.</h3>
+      <div className="media">
         <ul>
           {videos.map((video) => (
             <Video key={video.id} video={video} />
