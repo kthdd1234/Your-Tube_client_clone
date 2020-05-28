0451011d162aff7621bb43bc366a56cf78893dad
kthdd1234 <kthdd12@daum.net>
2020-05-28 14:24:56 +0900

    하람님 풀 받으세여

--- Main_Page/src/page/Header.js
+++ Main_Page/src/page/Header.js
@@ -12,14 +12,10 @@ export default class Header extends Component {
         >
           &#8801; 유저 정보 {/* &#8801; === 햄버거 */}
         </button>
-        <center>
-          <div
-            className={this.props.darkMode ? "YourTube darkMode" : "YourTube"}
-          >
-            <img src={logo} alt="YoutubeLogo" className="HeaderLogo" />
-            YourTube
-          </div>
-        </center>
+        <div className={this.props.darkMode ? "YourTube darkMode" : "YourTube"}>
+          <img src={logo} alt="YoutubeLogo" className="HeaderLogo" />
+          Your_Tube
+        </div>
       </div>
     );
   }
--- Main_Page/src/page/VideoPlayer.js
+++ Main_Page/src/page/VideoPlayer.js
@@ -2,24 +2,27 @@ import React, { Component } from "react"; //
 
 export default class VideoPlayer extends Component {
   render() {
-    const {
-      videoId,
-      title,
-      description,
-      thumbnail,
-      channelId,
-    } = this.props.clickVideo;
+    const { videoId, title, description, channelId } = this.props.clickVideo;
 
     return (
       <div
         className={this.props.darkMdoe ? "VideoPlayer darkMode" : "VideoPlayer"}
       >
+        <div>
+          <button
+            onClick={this.props.handleRemoveVideo}
+            style={{
+              float: "right",
+            }}
+          >
+            &times;
+          </button>
+        </div>
         <div className="">
-          {/* <iframe
+          <iframe
             src={`https://www.youtube.com/embed/${videoId}`}
             allowFullScreen
-          ></iframe> */}
-          <img className="thumbnails" src={thumbnail} alt="thumbnails"></img>
+          ></iframe>
           <h2>제목: {title}</h2>
           <div>채널: {channelId}</div>
           <div>설명: {description}</div>
