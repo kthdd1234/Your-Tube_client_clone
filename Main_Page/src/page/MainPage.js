36df9c68d3f8eaf3fa92b0253a2e3e0d35b1bfd3
jhin93 <scene1993@gmail.com>
2020-05-28 14:45:36 +0900

    설명50자로 줄이면 됩니다

--- Main_Page/src/page/MainPage.js
+++ Main_Page/src/page/MainPage.js
@@ -1,11 +1,10 @@
-
-import React from "react";
-import VideoList from "../page/VideoList";
-import { withRouter } from "react-router-dom";
-import axios from "axios";
-import Modal from "../page/Modal";
-import Header from "./Header";
-import VideoPlayer from "./VideoPlayer";
+import React from 'react';
+import VideoList from '../page/VideoList';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+import Modal from '../page/Modal';
+import Header from './Header';
+import VideoPlayer from './VideoPlayer';
 
 class MainPage extends React.Component {
   constructor(props) {
@@ -29,8 +28,7 @@ class MainPage extends React.Component {
   componentDidMount() {
     axios
       .post(
-
-        "http://ec2-3-34-122-219.ap-northeast-2.compute.amazonaws.com:4611/signin",
+        'http://ec2-54-180-121-14.ap-northeast-2.compute.amazonaws.com:4611/signin',
 
         {
           id: 1,
@@ -42,8 +40,7 @@ class MainPage extends React.Component {
       .then((data) => {
         axios
           .get(
-
-            "http://ec2-3-34-122-219.ap-northeast-2.compute.amazonaws.com:4611/list",
+            'http://ec2-54-180-121-14.ap-northeast-2.compute.amazonaws.com:4611/list',
 
             {
               headers: { 'x-api-key': data },
@@ -107,12 +104,12 @@ class MainPage extends React.Component {
             }}
           >
             <input
-              className={darkMode ? "SearchtBar darkMode" : "SearchtBar"}
+              className={darkMode ? 'SearchtBar darkMode' : 'SearchtBar'}
               placeholder="찾고 싶은 영상의 제목이나 단어를 입력하세요"
               onChange={this.handleInputValue}
             ></input>
             <button
-              className={darkMode ? "SearchtButton darkMode" : "SearchtButton"}
+              className={darkMode ? 'SearchtButton darkMode' : 'SearchtButton'}
               onClick={this.handleSearchData}
             >
               검색
@@ -122,7 +119,7 @@ class MainPage extends React.Component {
           {clickVideo ? (
             <VideoPlayer clickVideo={clickVideo} darkMode={darkMode} />
           ) : (
-            ""
+            ''
           )}
         </center>
         <div className="videoList">
@@ -133,7 +130,7 @@ class MainPage extends React.Component {
               hadleClickVideoPlayer={this.hadleClickVideoPlayer}
             />
           ) : (
-            ""
+            ''
           )}
         </div>
         <Modal
--- Main_Page/src/page/VideoPlayer.js
+++ Main_Page/src/page/VideoPlayer.js
@@ -1,25 +1,18 @@
-import React, { Component } from "react"; //
+import React, { Component } from 'react'; //
 
 export default class VideoPlayer extends Component {
   render() {
-    const {
-      videoId,
-      title,
-      description,
-      thumbnail,
-      channelId,
-    } = this.props.clickVideo;
+    const { videoId, title, description, channelId } = this.props.clickVideo;
 
     return (
       <div
-        className={this.props.darkMdoe ? "VideoPlayer darkMode" : "VideoPlayer"}
+        className={this.props.darkMdoe ? 'VideoPlayer darkMode' : 'VideoPlayer'}
       >
         <div className="">
-          {/* <iframe
+          <iframe
             src={`https://www.youtube.com/embed/${videoId}`}
             allowFullScreen
-          ></iframe> */}
-          <img className="thumbnails" src={thumbnail} alt="thumbnails"></img>
+          ></iframe>
           <h2>제목: {title}</h2>
           <div>채널: {channelId}</div>
           <div>설명: {description}</div>
